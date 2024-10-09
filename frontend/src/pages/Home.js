c165dd8f1fc7fcec21fa57b2f96dbbc7974305ed
T-blight <huynhdaithang01223@gmail.com>
2024-10-09 23:25:21 +0700

    update css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,9 +15,12 @@ const Home = () => {
                             <h1 className="banner-text">AUCTIONKOI</h1>
                         </div>
                     </div>
-                    <h1 className="banner-slogan">Kết Nối Trực Tiếp Với Những Người Nuôi Cá Koi Hàng Đầu
-                        <span> Nhật Bản</span>
-                    </h1>
+                    <div className="flexgrow">
+                        <h1 className="banner-slogan">Kết Nối Trực Tiếp Với Những Người Nuôi Cá Koi Hàng Đầu
+                            <span> Nhật Bản</span>
+                        </h1>
+                    </div>
+                    
                     <div className="button-container">
                         <a href="/auction" className="btn btn-Auction">View Auctions</a>
                         <a href="/help" className="btn btn-Learn">Learn More</a>
