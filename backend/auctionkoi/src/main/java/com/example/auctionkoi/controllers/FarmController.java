90c4729395cff953d95baacb7148f5656f12e23b
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2024-10-10 10:17:39 +0700

    fix thuoc tinh farm

--- backend/auctionkoi/src/main/java/com/example/auctionkoi/controllers/FarmController.java
+++ backend/auctionkoi/src/main/java/com/example/auctionkoi/controllers/FarmController.java
@@ -3,44 +3,34 @@ package com.example.auctionkoi.controllers;
 import com.example.auctionkoi.entities.Farm;
 import com.example.auctionkoi.services.FarmService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/farm")
-
 public class FarmController {
     @Autowired
     private FarmService farmService;
 
-    public FarmController(FarmService farmService) {
-        this.farmService = farmService;
-    }
-
-    @RequestMapping("/create")
-    public Farm create(Farm farm) {
-        return farmService.createFarm(farm);
-    }
-
-    @RequestMapping("/update")
-    public Farm update(Farm farm) {
-        return farmService.updateFarm(farm);
-    }
-
-    @RequestMapping("/delete")
-    public void delete(Long farmId) {
-        farmService.deleteFarm(farmId);
-    }
-
-    @RequestMapping("/get")
-    public Farm get(Long farmId) {
+    @GetMapping("/{farmId}")
+    public Farm getFarm(@PathVariable Long farmId) {
         return farmService.getFarm(farmId);
     }
 
-    @RequestMapping("/getAll")
-    public List<Farm> getAll() {
+    @GetMapping
+    public List<Farm> getAllFarms() {
         return farmService.getAllFarms();
     }
+
+    @PostMapping
+    public Farm createFarm(@RequestBody Farm farm) {
+        return farmService.createFarm(farm);
+    }
+
+    @DeleteMapping("/{farmId}")
+    public void deleteFarm(@PathVariable Long farmId) {
+        farmService.deleteFarm(farmId);
+    }
+
 }
--- backend/auctionkoi/src/main/java/com/example/auctionkoi/dto/request/FarmCreationRequest.java
+++ backend/auctionkoi/src/main/java/com/example/auctionkoi/dto/request/FarmCreationRequest.java
@@ -6,6 +6,7 @@ public class FarmCreationRequest {
     private String name;
     private String description;
     private String address;
+
     public String getName() {
         return name;
     }
